b3befcd63c40f6f0e22d91cb791cc355773c0f1a
doyelsaha510 <51129030+doyelsaha510@users.noreply.github.com>
2019-06-02 00:01:50 +0530

    navigation page

--- app/src/main/java/com/anshu/www/gdapp/navigationActivity.java
+++ app/src/main/java/com/anshu/www/gdapp/navigationActivity.java
@@ -1,21 +1,45 @@
 package com.anshu.www.gdapp;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.anshu.www.gdapp.model.MyDataModel;
+import com.anshu.www.gdapp.parser.JSONparser;
+import com.anshu.www.gdapp.utils.InternetConnection;
+import com.anshu.www.gdapp.utils.Keys;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+//import android.util.Keys;
 
 public class navigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ListView listView;
+    private ArrayList<MyDataModel> list;
+    private MyArrayAdapter adapter;
+    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +47,51 @@ public class navigationActivity extends AppCompatActivity
         setContentView(R.layout.activity_navigation);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        /**
+         * Array List for Binding Data from JSON to this List
+         */
+        list = new ArrayList<>();
+        /**
+         * Binding that List to Adapter
+         */
+        adapter = new MyArrayAdapter(this, list);
+
+        /**
+         * Getting List and Setting List Adapter
+         */
+        //id = R.layout.content_navigation;
+        listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Snackbar.make(findViewById(R.id.drawer_layout), list.get(position).getName() + " => " + list.get(position), Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        /**
+         * Just to know onClick and Printing Hello Toast in Center.
+         */
+        Toast toast = Toast.makeText(getApplicationContext(), "Click on FloatingActionButton to Load JSON", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+           //     Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+             //           .setAction("Action", null).show();
+                /**
+                 * Checking Internet Connection
+                 */
+
+                if (InternetConnection.checkConnection(getApplicationContext())) {
+                    new GetDataTask().execute();
+                } else {
+                    Snackbar.make(view, "Internet Connection Not Available", Snackbar.LENGTH_LONG).show();
+                }
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -39,7 +102,124 @@ public class navigationActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
     }
+    /**
+     * Creating Get Data Task for Getting Data From Web
+     */
+    class GetDataTask extends AsyncTask<Void, Void, Void> {
 
+        ProgressDialog dialog;
+        int jIndex;
+        int x;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /**
+             * Progress Dialog for User Interaction
+             */
+
+            x=list.size();
+
+            if(x==0)
+                jIndex=0;
+            else
+                jIndex=x;
+
+            dialog = new ProgressDialog(navigationActivity.this);
+            dialog.setTitle("Hey Wait Please..."+x);
+            dialog.setMessage("I am getting your JSON");
+            dialog.show();
+        }
+
+        @Nullable
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            /**
+             * Getting JSON Object from Web Using okHttp
+             */
+            JSONObject jsonObject = JSONparser.getDataFromWeb();
+
+            try {
+                /**
+                 * Check Whether Its NULL???
+                 */
+                if (jsonObject != null) {
+                    /**
+                     * Check Length...
+                     */
+                    if(jsonObject.length() > 0) {
+                        /**
+                         * Getting Array named "contacts" From MAIN Json Object
+                         */
+                        JSONArray array = jsonObject.getJSONArray(Keys.KEY_CONTACTS);
+
+                        /**
+                         * Check Length of Array...
+                         */
+
+
+                        int lenArray = array.length();
+                        if(lenArray > 0) {
+                            for( ; jIndex < lenArray; jIndex++) {
+
+                                /**
+                                 * Creating Every time New Object
+                                 * and
+                                 * Adding into List
+                                 */
+                                MyDataModel model = new MyDataModel();
+
+                                /**
+                                 * Getting Inner Object from contacts array...
+                                 * and
+                                 * From that We will get Name of that Contact
+                                 *
+                                 */
+                                JSONObject innerObject = array.getJSONObject(jIndex);
+                                String name = innerObject.getString(Keys.KEY_NAME);
+//                                String country = innerObject.getString(Keys.KEY_COUNTRY);
+
+                                /**
+                                 * Getting Object from Object "phone"
+                                 */
+                                //JSONObject phoneObject = innerObject.getJSONObject(Keys.KEY_PHONE);
+                                //String phone = phoneObject.getString(Keys.KEY_MOBILE);
+
+                                model.setName(name);
+                               // model.setCountry(country);
+
+                                /**
+                                 * Adding name and phone concatenation in List...
+                                 */
+                                list.add(model);
+                            }
+                        }
+                    }
+                } else {
+
+                }
+            } catch (JSONException je) {
+                Log.i(JSONparser.TAG, "" + je.getLocalizedMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            dialog.dismiss();
+            /**
+             * Checking if List size if more than zero then
+             * Update ListView
+             */
+            if(list.size() > 0) {
+                adapter.notifyDataSetChanged();
+            } else {
+                Snackbar.make(findViewById(R.id.drawer_layout), "No Data Found", Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/anshu/www/gdapp/utils/InternetConnection.java
@@ -0,0 +1,13 @@
+package com.anshu.www.gdapp.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.support.annotation.NonNull;
+
+public class InternetConnection {
+    /** CHECK WHETHER INTERNET CONNECTION IS AVAILABLE OR NOT */
+    public static boolean checkConnection(@NonNull Context context) {
+        return  ((ConnectivityManager) context.getSystemService
+                (Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo() != null;
+    }
+}
