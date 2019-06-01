16caa5954fadd496b71f875505b466d281bd9b02
priyanshukundu180 <priyanshukundu180@gmail.com>
2019-06-01 16:22:12 +0530

    added the navigation activity

--- app/src/main/java/com/anshu/www/gdapp/MainActivity.java
+++ app/src/main/java/com/anshu/www/gdapp/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
                             if (!task.isSuccessful()) {
                                 Toast.makeText(MainActivity.this, "Not sucessfull", Toast.LENGTH_SHORT).show();
                             } else {
-                                startActivity(new Intent(MainActivity.this, logoutActivity.class));
+                                startActivity(new Intent(MainActivity.this, navigationActivity.class));
                             }
                         }
                     });
